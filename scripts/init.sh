9ea3b4cf7dc1b79d46365294149005c5753f44e1
Rich Snapp <rich.snapp@gmail.com>
2013-05-29 15:36:55 -0600

    Revert "don't need init script anymore"
    This reverts commit 4278ea3eaa01c3317869e714ad3f91a28d0a8c06.
    
    It looks like we did need it after all!

---
+++ scripts/init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# requires root access
+
+cd $1/$2
+
+. /etc/lsb-release
+
+DEB="puppetlabs-release-${DISTRIB_CODENAME}.deb"
+DEB_PROVIDES="/etc/apt/sources.list.d/puppetlabs.list" # Assume that this file's existence means we have the Puppet Labs repo added
+
+if [ ! -e $DEB_PROVIDES ]
+then
+    print "Could not find $DEB_PROVIDES - fetching and installing $DEB"
+    wget -q http://apt.puppetlabs.com/$DEB
+    dpkg -i $DEB
+
+    apt-get update
+    apt-get install --yes puppet
+    apt-get -f install --yes
+
+    rm $DEB
+
+    cat >/etc/apt/preferences.d/00-puppet.pref <<EOL
+Package: puppet puppet-common
+Pin: version 3.1*
+Pin-Priority: 501
+EOL
+
+    # update sudoers to allow deploy to run deploy script w/o sudo password
+    if [ -e /tmp/sudoers.tmp ]
+    then
+        rm /tmp/sudoers.tmp
+    fi
+    cp /etc/sudoers /tmp/sudoers.tmp
+    echo "deploy ALL=(ALL) NOPASSWD:$1/deploy_$2.sh" >> /tmp/sudoers.tmp
+    visudo -c -f /tmp/sudoers.tmp
+    if [ "$?" -eq "0" ]
+    then
+        cp /tmp/sudoers.tmp /etc/sudoers
+    fi
+    rm /tmp/sudoers.tmp
+fi
+
+puppet apply --environment $2 --hiera_config "$1/$2/env/config/hiera.yaml" --modulepath "$1/$2/env/modules" --templatedir "$1/$2/config/templates" $1/$2/env/manifests/site.pp
